a26bda756df37c85f560ef2fe2bd1fbd4a114df0
Espanet Florent <florent.esp@gmail.com>
2016-09-09 22:55:28 +0200

    Update Move bound mousebutton

--- src/Events/Listeners/Move.cpp
+++ src/Events/Listeners/Move.cpp
@@ -9,7 +9,7 @@ void Listener::Move::onEvent(const sf::Event &event, sf::View &view, sf::RenderW
 {
   if (event.type == sf::Event::MouseButtonPressed)
     {
-      if (event.mouseButton.button == sf::Mouse::Left)
+      if (event.mouseButton.button == sf::Mouse::Right)
         {
           _isMoving = true;
           _movingFrom.x = event.mouseButton.x;
@@ -18,7 +18,7 @@ void Listener::Move::onEvent(const sf::Event &event, sf::View &view, sf::RenderW
     }
   else if (event.type == sf::Event::MouseButtonReleased)
     {
-      if (event.mouseButton.button == sf::Mouse::Left)
+      if (event.mouseButton.button == sf::Mouse::Right)
         {
           _isMoving = false;
         }
