8cc9718d1b7db12d999c940e8948f24404ed34e7
Florent Espanet <florent@lumapps.com>
2016-10-04 13:51:16 +0200

    Reformat CreatePlanet listener

--- src/Events/Listeners/CreatePlanet.cpp
+++ src/Events/Listeners/CreatePlanet.cpp
@@ -12,46 +12,35 @@ Listener::CreatePlanet::CreatePlanet(const std::vector<sf::Event::EventType> &ev
 
 void Listener::CreatePlanet::onEvent(const sf::Event &event, sf::View &view, sf::RenderWindow &window, Universe &universe)
 {
-  if (event.type == sf::Event::MouseButtonPressed)
+  if (event.type == sf::Event::MouseButtonPressed && event.mouseButton.button == sf::Mouse::Left && !universe.getObjectAtCursor())
     {
-      if (event.mouseButton.button == sf::Mouse::Left)
-        {
-          if (!universe.getObjectAtCursor())
-            {
-              auto mousePosition = sf::Vector2<int>(event.mouseButton.x, event.mouseButton.y);
-              auto planetPosition = sf::Vector2<float>(window.mapPixelToCoords(mousePosition, view));
+      auto mousePosition = sf::Vector2<int>(event.mouseButton.x, event.mouseButton.y);
+      auto planetPosition = sf::Vector2<float>(window.mapPixelToCoords(mousePosition, view));
 
-              auto *planet = new Planet(10.f, 1, planetPosition);
-              planet->setFillColor(sf::Color::Blue);
-              _movingObject = planet;
+      auto *planet = new Planet(10.f, 1, planetPosition);
+      planet->setFillColor(sf::Color::Blue);
+      _movingObject = planet;
 
-              auto *line = new sf::VertexArray(sf::Lines, 2);
-              (*line)[0].position = planetPosition;
-              (*line)[1].position = planetPosition;
-              _speedLine = line;
-              universe.addOverlay(_speedLine);
-            }
-        }
+      auto *line = new sf::VertexArray(sf::Lines, 2);
+      (*line)[0].position = planetPosition;
+      (*line)[1].position = planetPosition;
+      _speedLine = line;
+      universe.addOverlay(_speedLine);
     }
-  else if (event.type == sf::Event::MouseButtonReleased)
+  else if (event.type == sf::Event::MouseButtonReleased && event.mouseButton.button == sf::Mouse::Left && _movingObject)
     {
-      if (event.mouseButton.button == sf::Mouse::Left)
-        {
-          universe.addObject(_movingObject);
-          _movingObject = NULL;
-          universe.removeOverlay(_speedLine);
-          _speedLine = NULL;
-        }
+      universe.addObject(_movingObject);
+      _movingObject = NULL;
+
+      universe.removeOverlay(_speedLine);
+      _speedLine = NULL;
     }
-  else if (event.type == sf::Event::MouseMoved)
+  else if (event.type == sf::Event::MouseMoved && _movingObject)
     {
       auto mousePosition = sf::Vector2<int>(event.mouseMove.x, event.mouseMove.y);
       auto objectPosition = sf::Vector2<float>(window.mapPixelToCoords(mousePosition, view));
 
-      if (_movingObject)
-        {
-          (*_speedLine)[1].position = objectPosition;
-          _movingObject->setSpeed((objectPosition - _movingObject->getPosition()) / 1000.f);
-        }
+      (*_speedLine)[1].position = objectPosition;
+      _movingObject->setSpeed((objectPosition - _movingObject->getPosition()) / 1000.f);
     }
 }
